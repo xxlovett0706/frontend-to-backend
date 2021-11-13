be320a7337ea8bab360fa2f732dd93c2b0bbb772
MasK <xxlovett0706@gmail.com>
2021-11-14 00:41:05 +0800

    update: Golang

---
+++ Stage2/Golang/01-helloworld/go.mod
@@ -0,0 +1,5 @@
+module 01-helloworld
+
+go 1.17
+
+require github.com/q1mi/hello v0.1.1
---
+++ Stage2/Golang/01-helloworld/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/q1mi/hello"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+	hello.SayHi()
+}
