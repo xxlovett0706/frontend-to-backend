82b48d20bb93eb46e7646b4ca375c88673793e45
MasK <xxlovett0706@gmail.com>
2021-11-23 19:22:01 +0800

    update: Golang

---
+++ Stage2/Golang/Go指南/01-基础/19-for循环（续2）/main.go
@@ -0,0 +1,14 @@
+/*
+* 此时你可以去掉分号，因为 C 的 while 在 Go 中叫做 for。
+ */
+package main
+
+import "fmt"
+
+func main() {
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+}
---
+++ Stage2/Golang/Go指南/01-基础/23-if和else/main.go
@@ -0,0 +1,11 @@
+/*
+	* 在 if 的简短语句中声明的变量同样可以在任何对应的 else 块中使用。
+
+	! 注意事项
+	* 在 main 的 fmt.Println 调用开始前，两次对 pow 的调用均已执行并返回其各自的结果。
+*/
+package main
+
+func main() {
+
+}
