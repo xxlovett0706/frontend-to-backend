f37b766e9cd3e4588692007ac792d329c3cddc30
MasK <xxlovett0706@gmail.com>
2021-11-25 20:34:36 +0800

    update: Golang

--- Stage2/Golang/Go指南/01-基础/25-switch的求值顺序/main.go
+++ Stage2/Golang/Go指南/01-基础/25-switch的求值顺序/main.go
@@ -5,3 +5,23 @@
 	* Go 练习场中的时间总是从 2009-11-10 23:00:00 UTC 开始，该值的意义留给读者去发现。
 */
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today")
+	case today + 1:
+		fmt.Println("Tomorrow")
+	case today + 2:
+		fmt.Println("In two days")
+	default:
+		fmt.Println("Too far away.")
+	}
+}
